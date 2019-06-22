5ca547955f7189138c3c92af4a4a8a221211e031
Manel Matougui <m.matougui@gmail.com>
2019-06-21 18:50:41 -0700

    Building the game’s menu

---
+++ SnakeGame/SnakeGame/GameManager.swift
@@ -0,0 +1,13 @@
+//
+//  GameManager.swift
+//  SnakeGame
+//
+//  Created by Manel matougui on 6/21/19.
+//  Copyright © 2019 udacity. All rights reserved.
+//
+
+import Foundation
+class GameManager {
+    
+    
+}
